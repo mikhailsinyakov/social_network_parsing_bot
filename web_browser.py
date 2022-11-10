e5a843893c8115e8b4603fa86f075f198ab4edf7
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2022-11-10 13:18:17 +0600

    Fix some bugs that appeared when working inside docker container

--- web_browser.py
+++ web_browser.py
@@ -9,6 +9,7 @@ def get_webdriver():
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(options=chrome_options)
     return driver
@@ -18,11 +19,12 @@ def get_tik_tok_video_src(url):
     driver.get(url)
     try:
         video_elements = driver.find_elements(By.TAG_NAME, "video")
-        if len(video_elements) != 1:
-            return None
-        video_element = driver.find_element(By.TAG_NAME, "video")
     except NoSuchElementException:
         return None
+        
+    if len(video_elements) != 1:
+        return None
+    video_element = video_elements[0]
     video_src = video_element.get_attribute("src")
 
     return video_src
